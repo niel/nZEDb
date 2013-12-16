712028f4dc3aa3f465b4e2198769d188465204ec
niel <niel.archer@gmail.com>
2013-12-16 03:51:44 +0000

    Adding Sites model/controller used by Pages controller.

---
+++ controllers/SitesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2013, nZEDb (http://nzedb.com)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_nzedb\controllers;
+
+class SitesController extends \lithium\action\Controller
+{
+}
+
+?>
