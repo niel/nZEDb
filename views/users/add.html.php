306404b517dea305505b8f3ea4699a723dedd495
niel <niel.archer@gmail.com>
2013-12-20 07:12:31 +0000

    Adding Users model/controller/views

---
+++ views/users/add.html.php
@@ -0,0 +1,8 @@
+<h2>Add user</h2>
+<?=$this->form->create($user); ?>
+	<?=$this->form->field('username'); ?>
+	<?=$this->form->field('firstname'); ?>
+	<?=$this->form->field('lastname'); ?>
+	<?=$this->form->field('password', array('type' => 'password')); ?>
+	<?=$this->form->submit('Create me'); ?>
+<?=$this->form->end(); ?>
