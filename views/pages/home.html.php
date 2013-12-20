7d5748d66e30577e0f96e2d28a89f7bc422317dc
niel <niel.archer@gmail.com>
2013-12-20 14:31:35 +0000

    Adding?changing License headers

--- views/pages/home.html.php
+++ views/pages/home.html.php
@@ -1,10 +1,20 @@
 <?php
-/**
- * Lithium: the most rad php framework
- *
- * @copyright     Copyright 2013, Union of RAD (http://union-of-rad.org)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
- */
+/*
+Copyright (C) 2013 nZEDb
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 use lithium\core\Libraries;
 use lithium\core\Environment;
