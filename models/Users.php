3a439619a580e6441a7997c729cab2db8e2da961
niel <niel.archer@gmail.com>
2013-12-20 13:02:04 +0000

    Adding a utility method to combine first and last names into fullName.

--- models/Users.php
+++ models/Users.php
@@ -4,6 +4,10 @@ namespace li3_nzedb\models;
 
 class Users extends \lithium\data\Model
 {
+	public function fullName($entity)
+	{
+		return $entity->firstName . ' ' . $entity->lastName;
+	}
 }
 
 ?>
