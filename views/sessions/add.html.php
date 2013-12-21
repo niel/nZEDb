aeb9a3ab06fbcd4df32c5d63cfb1f2a407872348
niel <niel.archer@gmail.com>
2013-12-21 23:24:33 +0000

    Adding div block around login to allow styling.

--- views/sessions/add.html.php
+++ views/sessions/add.html.php
@@ -1,5 +1,7 @@
+<div id="login">
 <?=$this->form->create(null); ?>
     <?=$this->form->field('username'); ?>
     <?=$this->form->field('password', array('type' => 'password')); ?>
     <?=$this->form->submit('Log in'); ?>
 <?=$this->form->end(); ?>
+</div>
