f697fa3ce67286abc8a04df3bebacaf2f32abe03
niel <niel.archer@gmail.com>
2013-12-22 03:20:03 +0000

    Changing $auth to $user as that's the data it contains.

--- views/pages/home.html.php
+++ views/pages/home.html.php
@@ -1,27 +1,25 @@
 <?php
 /**
- * Copyright (C) 2013 nZEDb
+ * Lithium: the most rad php framework
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program (see LICENSE.txt in the base directory.  If
- * not, see <http://www.gnu.org/licenses/>.
+ * @copyright     Copyright 2013, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 use lithium\core\Libraries;
 use lithium\core\Environment;
 use lithium\data\Connections;
 
+use \li3_nzedb\models\Sites;
+
 $this->title('Home');
 $this->html->style('debug', array('inline' => false));
 
+if (!$user) {
+	echo Sites::get('home-content');
+} else {
+
+}
+
 ?>
+
