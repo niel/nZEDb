5aaf0bd6e2b08a9a82c782cdc15174a0e1358e30
niel <niel.archer@gmail.com>
2013-12-22 03:20:41 +0000

    Adding ::get() method to retrieve specific setting value.

--- models/Sites.php
+++ models/Sites.php
@@ -1,20 +1,9 @@
 <?php
 /**
- * Copyright (C) 2013 nZEDb
+ * Lithium: the most rad php framework
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program (see LICENSE.txt in the base directory.  If
- * not, see <http://www.gnu.org/licenses/>.
+ * @copyright     Copyright 2013, nZEDb (http://nzedb.com)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 namespace li3_nzedb\models;
@@ -23,15 +12,11 @@ class Sites extends \lithium\data\Model
 {
 	protected $_meta = array('source' => 'site');
 
-	static public function setting($setting)
+	static public function get($setting)
 	{
-		$result = self::find(array(
-			'fields' => array('setting', 'value'),
-			'conditions' => array(
-				'setting' => array('=' => $setting)
-			)
-		));
-		return $result->data('value');
+		$setting = self::find('first', array('conditions' => array('setting' => array('=' => $setting))));
+		$setting = $setting == true ? $setting->data()['value'] : null;
+		return $setting;
 	}
 }
 
