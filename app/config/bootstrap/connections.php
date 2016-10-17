ec623d7ae242b81f5008412a79eaa7eb1c5d0933
niel <niel.archer@gmail.com>
2016-10-17 15:13:55 +0100

    Better, better fix for that constant thingy.

--- app/config/bootstrap/connections.php
+++ app/config/bootstrap/connections.php
@@ -111,9 +111,12 @@ if (file_exists($config) && !defined('nZEDb_INSTALLER')) {
 		);
 	}
 
-	define('nZEDb_COVERS',
-		realpath(nZEDb_ROOT .
-			nzedb\utility\Text::trailingSlash(app\models\Settings::value('site.main.coverspath'))));
+	if (!defined('nZEDb_INSTALLER'  || nZEDb_INSTALLER !== true)) {
+		define('nZEDb_COVERS',
+			realpath(nZEDb_ROOT .
+				nzedb\utility\Text::trailingSlash(app\models\Settings::value('site.main.coverspath'))));
+	}
+
 } else {
 	/** throw new ErrorException("Couldn't open nZEDb's configuration file!"); */
 	Connections::add('default',
